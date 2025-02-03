d958f1153677573e5d1dc596b240c1f4aa0b326e
Coder2003Anujyoti <anujyotide@gmail.com>
2025-02-03 13:02:56 +0530

    Changes

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import {HashLink} from 'react-router-hash-link'
 import {useSearchParams,Link} from "react-router-dom"
 const TeamDetails = () => {

b16e33f23b4602627ee3210fe91b54c43452f587
Coder2003Anujyoti <anujyotide@gmail.com>
2025-02-04 16:06:54 +0530

    Changes

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -6,10 +6,15 @@ const TeamDetails = () => {
   const teamId = searchParams.get("team"); 
   const teams=["Mi","Csk","Rr","Kkr","Gt","Pbks","Rcb","Lsg","Dc","Srh"];
   const [load,setLoad]=useState(true);
+    const [item,setItem]=useState([]);
+  const get_data=async()=>{
+    const response=await fetch('https://prepared-josy-handcricket-0e7a326f.koyeb.app/');
+    const data=await response.json();
+    setItem(data.data);
+    setLoad(false);
+  }
   useEffect(()=>{
-    setTimeout(function() {
-      setLoad(false)
-    },1000);
+    get_data();
   },[])
   useEffect(()=>{
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -75,6 +80,41 @@ const TeamDetails = () => {
     </div>
     </Link>
 </div>
+  <div className="w-full py-4 flex justify-center">
+    <h1 className="text-xl font-extrabold text-slate-400">Top Batters</h1>
+  </div>
+  <div className="w-full flex flex-col justify-center gap-4 ">
+    
+    {
+      item.sort((a,b)=>b.runs-a.runs).map((i,ind)=>{
+      if(ind<=2)
+        return(<>
+ <div className="w-full flex flex-row flex-wrap justify-evenly border-b p-4 border-b-slate-400 border-t-transparent border-l-transparent border-r-transparent">
+   <img src={i.image} className="w-20 h-20"/>
+  <div className="flex justify-center items-center"><h2 className="text-sm font-extrabold text-slate-400 ">{i.name}</h2></div>
+    <div className="flex justify-center items-center"> <h2 className="text-sm font-extrabold text-slate-400 ">Runs-:{i.runs}</h2></div>
+   </div>
+        </>)
+      })
+    }
+    </div>
+    <div className="w-full py-8 flex justify-center">
+    <h1 className="text-xl font-extrabold text-slate-400">Top Bowlers</h1>
+  </div>
+  <div className="w-full flex flex-col justify-center gap-4 ">
+    {
+      item.sort((a,b)=>b.wickets-a.wickets).map((i,ind)=>{
+      if(ind<=2)
+        return(<>
+ <div className="w-full flex flex-row flex-wrap justify-evenly border-b p-4 border-b-slate-400 border-t-transparent border-l-transparent border-r-transparent">
+   <img src={i.image} className="w-20 h-20"/>
+  <div className="flex justify-center items-center"><h2 className="text-sm font-extrabold text-slate-400 ">{i.name}</h2></div>
+    <div className="flex justify-center items-center"> <h2 className="text-sm font-extrabold text-slate-400 ">Wickets-:{i.wickets}</h2></div>
+   </div>
+        </>)
+      })
+    }
+    </div>
     <footer className="bg-black text-white">
       <div className="w-full flex justify-center  text-center flex-col p-4 mt-4">
         <h2 className="text-xl font-semibold">Quick Links</h2>

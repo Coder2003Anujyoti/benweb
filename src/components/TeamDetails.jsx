ec20dbe19857c160024429604b4ab542d499b7d2
Coder2003Anujyoti <anujyotide@gmail.com>
2025-02-06 22:04:53 +0530

    Changes

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -101,7 +101,7 @@ const TeamDetails = () => {
       })
     }
     </div>
-    <div className="w-full py-8 flex justify-center">
+    <div className="w-full py-4 flex justify-center">
     <h1 className="text-xl font-extrabold text-slate-400">Top Bowlers</h1>
   </div>
   <div className="w-full flex flex-col justify-center gap-4 ">

2df800d8d8db4e818cc4ad0a0a07da3b9f9fd3de
Coder2003Anujyoti <anujyotide@gmail.com>
2025-03-14 22:08:36 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 const iplRoutes = require('./files/ipl.js');
 const cors=require('cors');
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
 app.use(cors({
   origin:"*"
 }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true}));
 app.use(iplRoutes);
 const PORT = 8000;
 app.listen(PORT, () => {

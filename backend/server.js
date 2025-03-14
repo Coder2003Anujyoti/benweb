c87bb93483cbd2019ace963403fff127f8ee11b0
Coder2003Anujyoti <anujyotide@gmail.com>
2025-03-14 21:50:36 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const iplRoutes = require('./files/ipl.js');
 const cors=require('cors');
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:false}));
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
 app.use(cors({
   origin:"*"
 }));
